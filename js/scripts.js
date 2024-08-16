c7508067edb27bb1f2b4b9b16e9264ee560b8666
NullCipherr <blendsteal@gmail.com>
2024-08-16 20:22:36 -0300

    add base codes.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function sendEmail() {
+    const email = 'blendsteal@gmail.com'; // Substitua pelo seu endereço de e-mail
+    const subject = 'Hire Me Request';
+    const body = 'Hello, I would like to hire you.';
+
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+}
