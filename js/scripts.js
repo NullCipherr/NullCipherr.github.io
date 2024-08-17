d8d19ade10e11bfea26e43522d42cbbbc28482c6
NullCipherr <blendsteal@gmail.com>
2024-08-17 19:08:48 -0300

    update codes - f - b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    const h1Element = document.querySelector('.profile h1');
+    const text = " > Andrei Costa";
     // Simula o carregamento da página com um atraso artificial
     setTimeout(function() {
         // Remove o overlay de carregamento
@@ -9,6 +11,7 @@ window.onload = function() {
         content.style.opacity = '1';
         content.style.visibility = 'visible';
         content.style.transition = 'opacity 1s ease-in-out';
+        typeWriter(h1Element, text, 100);
         
     }, 2000); // Atraso de 2 segundos
 };
@@ -27,3 +30,15 @@ function downloadResume() {
     window.location.href = '/files/curriculo.pdf';
 }
 
+function typeWriter(element, text, delay = 100) {
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, delay);
+        }
+    }
+    element.innerHTML = ""; // Limpa o texto antes de começar a digitação
+    type();
+}
