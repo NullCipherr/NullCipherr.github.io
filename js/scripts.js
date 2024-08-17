7ca6f8ca4bff32b63aea2d4c2630ffbef20e02d0
NullCipherr <blendsteal@gmail.com>
2024-08-17 18:37:41 -0300

    update codes - f - b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+window.onload = function() {
+    // Simula o carregamento da página com um atraso artificial
+    setTimeout(function() {
+        // Remove o overlay de carregamento
+        document.getElementById('loading-overlay').style.display = 'none';
+
+        // Inicia a animação de fade-in
+        var content = document.getElementById('content');
+        content.style.opacity = '1';
+        content.style.visibility = 'visible';
+        content.style.transition = 'opacity 1s ease-in-out';
+        
+    }, 2000); // Atraso de 2 segundos
+};
+
 function sendEmail() {
     const email = 'blendsteal@gmail.com'; // Substitua pelo seu endereço de e-mail
     const subject = 'Hire Me Request';
